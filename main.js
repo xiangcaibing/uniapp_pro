e858cd4dbebba36c835107398a835051b659ca09
xiangcai <575995132@qq.com>
2023-06-13 22:40:45 +0800

    home finished

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
+// 导入网络请求包
+import { $http } from '@escook/request-miniprogram'
 
+uni.$http = $http
+// 请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+// 响应拦截器
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+// 封装弹框的方法
+uni.$showMsg = function(title='数据请求失败！', duration=1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
 
 Vue.config.productionTip = false
 
